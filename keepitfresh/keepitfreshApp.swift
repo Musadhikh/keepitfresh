168c277970bc19749e842cfdf18c010b44071d17
Musadhikh Muhammed K <Musadhikh@users.noreply.github.com>
2025-08-31 11:51:46 +0800

    Initial Commit

---
+++ keepitfresh/keepitfreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepitfreshApp.swift
+//  keepitfresh
+//
+//  Created by Musadhikh Muhammed K on 31/8/25.
+//
+
+import SwiftUI
+
+@main
+struct keepitfreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ keepitfreshTests/keepitfreshTests.swift
@@ -0,0 +1,16 @@
+//
+//  keepitfreshTests.swift
+//  keepitfreshTests
+//
+//  Created by Musadhikh Muhammed K on 31/8/25.
+//
+
+import Testing
+
+struct keepitfreshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
