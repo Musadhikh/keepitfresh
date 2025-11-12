4117a82c799e6a4e5b5e9f99a193523ea2066a60
Musadhikh <musadhikh.muhammed@accedo.tv>
2025-11-12 17:24:50 +0800

    App Metadata service added

--- keepitfresh/Domain/Models/AppMetadata.swift
+++ keepitfresh/Domain/Models/AppMetadata.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
-struct AppMetadata: Codable, Equatable, Sendable {
+struct AppMetadata: Codable, Equatable, Sendable, CustomStringConvertible {
     let message: String
     let minimumVersion: String
     let appStoreUrl: String
     let isUnderMaintenance: Bool
+    
+    var description: String {
+        "AppMetadata(message: \"\(message)\", minimumVersion: \(minimumVersion), appStoreUrl: \(appStoreUrl), isUnderMaintenance: \(isUnderMaintenance))"
+    }
 }
