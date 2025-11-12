41266ef54e0fdacd3d4d5cb307d99350b48e667d
Musadhikh <musadhikh.muhammed@accedo.tv>
2025-11-12 13:47:00 +0800

    App launch use case defined

--- keepitfresh/Domain/UseCases/AppLaunch/AppLaunchUseCase.swift
+++ keepitfresh/Domain/UseCases/AppLaunch/AppLaunchUseCase.swift
@@ -9,13 +9,43 @@ import Foundation
 
 struct AppLaunchUseCase {
     
-    func execute() async throws {
+    let metadataProvider: any AppMetadataProviding
+    let versionCheckProvider: any VersionCheckProviding
+    let userProvider: any UserProviding
+    let profileProvider: any UserProfileProviding
+    
+    func execute() async throws -> AppLaunchState {
         /// 1. Refresh app metadata
+        let metadata = try await metadataProvider.getAppMetadata()
+        
         /// 2. Check for maintenance
+        if metadata.isUnderMaintenance {
+            return AppLaunchState.maintenance
+        }
         /// 3. Check for App Version update
+        if versionCheckProvider.requiresVersionUpdate(metadata: metadata) {
+            return AppLaunchState.updateRequired
+        }
+        
         /// 4. Check is user logged in
+        guard let user = try await userProvider.current() else {
+            return AppLaunchState.loginRequired
+        }
         /// 5. Refresh user session
+        try await userProvider.validateSession()
+        
         /// 6. Refresh and get Profile
+        let profile = try await profileProvider.getUserProfile(for: user.id)
+        
         /// 7. Check if has valid house holds and last selected house hold
+        if profile.householdIds.isEmpty {
+            return AppLaunchState.createHousehold
+        }
+        
+        if profile.lastSelectedHouseholdId == nil {
+            return AppLaunchState.selectHousehold
+        }
+        
+        return .mainContent
     }
 }
