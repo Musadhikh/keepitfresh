fdaab2840614e2970a47952c9df0ce623599eaeb
Musadhikh <musadhikh.muhammed@accedo.tv>
2026-01-07 17:01:11 +0800

    Login View added

---
+++ keepitfresh/Domain/UseCases/Login/LoginUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  LoginUseCase.swift
+//  keepitfresh
+//
+//  Created by Musadhikh Muhammed on 14/11/25.
+//
+
+import Foundation
+
+struct LoginUseCase: Sendable {
+    
+    func execute() {
+        /// 1. login
+        /// 2. fetch profile
+        /// 3. create profile if required
+        /// 4. determine next step
+    }
+}
---
+++ keepitfresh/Presentation/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  keepitfresh
+//
+//  Created by Musadhikh Muhammed on 14/11/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+#Preview {
+    LoginView()
+}
+#endif
